58c8ba63804c6952dbd7169e49443d25e710cd60
TriBhaskar <panthribhaskar@gmail.com>
2025-04-08 22:38:17 +0530

    feat: add ForgotPasswordForm component and integrate it into ForgotPasswordPage

---
+++ src/components/layout/ForgotPasswordForm.tsx
@@ -0,0 +1,5 @@
+export default function ForgotPasswordForm() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10"></div>
+  );
+}
---
+++ src/pages/ForgotPasswordPage.tsx
@@ -0,0 +1,9 @@
+import ForgotPasswordForm from "@/components/layout/ForgotPasswordForm";
+
+export default function ForgotPasswordPage() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <ForgotPasswordForm />
+    </div>
+  );
+}
